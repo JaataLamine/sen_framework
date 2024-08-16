28ba873ba693e61e861073f58bc37b8b6411294e
JaataLamine <alamine1409@gmail.com>
2024-08-16 16:45:29 +0000

    Error reporting with ignition

--- app/Core/App.php
+++ app/Core/App.php
@@ -2,10 +2,13 @@
 
 namespace App\Core;
 
+use Symfony\Component\VarDumper\VarDumper;
+
 class App
 {
   public function run()
   {
+    var_dump($this->name);
     var_dump("run app");
   }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit35af080a0bf22936c4aca891b98c3fc6
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit35af080a0bf22936c4aca891b98c3fc6', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit35af080a0bf22936c4aca891b98c3fc6', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit35af080a0bf22936c4aca891b98c3fc6
         call_user_func(\Composer\Autoload\ComposerStaticInit35af080a0bf22936c4aca891b98c3fc6::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit35af080a0bf22936c4aca891b98c3fc6::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
